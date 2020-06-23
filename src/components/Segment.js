920156a8d3e74c9c2b6a6c32d50379b598d4aa45
tttrung <tttrung@fossil.com>
2020-06-23 20:43:49 +0700

    TRIVIAL: refactor code

--- src/components/Segment.js
+++ src/components/Segment.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const defaultProps = {
-  timeline: {start: 0, end: 0},
+  timeline: { start: 0, end: 0 },
   line: 0
-}
+};
 
 const Segment = (props = defaultProps) => {
-  const {line, segment, totalTrackLength} = props
-  const width = (segment.end - segment.start)*100/totalTrackLength
-  const left = segment.start*100/totalTrackLength
+  const { line, segment, totalTrackLength } = props;
+  const width = ((segment.end - segment.start) * 100) / totalTrackLength;
+  const left = (segment.start * 100) / totalTrackLength;
 
   return (
     <div
-      className='segment'
-      style={{width: `${width}%`, left: `${left}%`, top: `${20*line}px`}}
+      className="segment"
+      style={{ width: `${width}%`, left: `${left}%`, top: `${20 * line}px` }}
     >
       <span>{segment.start}</span>
       <span>{segment.end}</span>
     </div>
-  )
-}
+  );
+};
 
-export default Segment
+export default Segment;
--- src/components/TimelineSegments.js
+++ src/components/TimelineSegments.js
@@ -1,58 +1,66 @@
 import React, { useState, useEffect } from "react";
 
-import Segment from './Segment'
+import Segment from "./Segment";
 
 // assume all data is valid and sorted by start time
-const TimelineSegments = ({data = [], totalTrackLength = 0}) => {
-  const [segments, setSegments] = useState()
-  const [countLine, setCountLine] = useState(0)
+const TimelineSegments = ({ data = [], totalTrackLength = 0 }) => {
+  const [segments, setSegments] = useState();
+  const [countLine, setCountLine] = useState(0);
 
   useEffect(() => {
-    sortData(data)
-    let lines = []
+    sortData(data);
+    let lines = [];
 
-    lines.push([data[0]])
+    lines.push([data[0]]);
     // generate structure of timelines with time not overlap
     for (let i = 1; i < data.length; i++) {
       for (let key = 0; key < lines.length; key++) {
-        let len = lines[key].length
+        let len = lines[key].length;
         if (lines[key][len - 1].end <= data[i].start) {
-          lines[key].push(data[i])
-          break
+          lines[key].push(data[i]);
+          break;
         }
         if (key === lines.length - 1) {
-          lines.push([data[i]])
-          break
+          lines.push([data[i]]);
+          break;
         }
       }
     }
 
-    setCountLine(lines.length)
-    setSegments(generateSegment(lines))
-  }, [data])
-
-  const generateSegment = (lines) => {
-    let segments = []
-    for (let i = 0; i < lines.length; i++) {
-      for (let j = 0; j < lines[i].length; j++) {
-        const segment = lines[i][j]
-        segment && segments.push(<Segment key={segment.id} segment={segment} line={i} totalTrackLength={totalTrackLength}/>)
-      }
-    }
-    return segments
-  }
+    setCountLine(lines.length);
+    setSegments(generateSegment(lines, totalTrackLength));
+  }, [data, totalTrackLength]);
 
   return (
-    <div className="container" style={{height: `${30*countLine}px`}}>
-        {segments}
+    <div className="container" style={{ height: `${30 * countLine}px` }}>
+      {segments}
     </div>
-  )
+  );
 };
 
-const sortData = (data) => {
+const generateSegment = (lines, totalTrackLength) => {
+  let segments = [];
+  for (let i = 0; i < lines.length; i++) {
+    for (let j = 0; j < lines[i].length; j++) {
+      const segment = lines[i][j];
+      segment &&
+        segments.push(
+          <Segment
+            key={segment.id}
+            segment={segment}
+            line={i}
+            totalTrackLength={totalTrackLength}
+          />
+        );
+    }
+  }
+  return segments;
+};
+
+const sortData = data => {
   data.sort((a, b) => {
-    return a.start - b.start || (a.start === b.start && a.end - b.end)
-  })
-}
+    return a.start - b.start || (a.start === b.start && a.end - b.end);
+  });
+};
 
 export default TimelineSegments;
